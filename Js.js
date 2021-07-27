467a33f51e7a2dbb5df75301c21eb71340e0f79f
AlistairW <alistairwileman@gmail.com>
2021-07-27 08:41:29 +0200

    Added typing animation
    H3 removed and pushed in through JS

--- Js.js
+++ Js.js
@@ -8,4 +8,15 @@ function w3_open() {
     document.getElementById("myOverlay").style.display = "none";
   }
   
+  var i=0;
+  text = "How can we help you?";
   
+  function typing(){
+    if(i<text.length){
+      document.getElementById("typo").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typing,90);
+    }
+  }
+
+  typing();
