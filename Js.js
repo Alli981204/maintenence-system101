f3c0f2fc08bc65d89f401157eada00d7999566aa
AlistairW <alistairwileman@gmail.com>
2021-08-12 09:44:33 +0200

    Revert "uptoDate"
    This reverts commit 67542dd4fe0da2437512f3454d33639c4484adc7.

--- Js.js
+++ Js.js
@@ -1,23 +1,44 @@
-// function w3_open() {
-//     document.getElementById("mySidebar").style.display = "block";
-//     document.getElementById("myOverlay").style.display = "block";
-//   }
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
    
-//   function w3_close() {
-//     document.getElementById("mySidebar").style.display = "none";
-//     document.getElementById("myOverlay").style.display = "none";
-//   }
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
   
+
+
+
+
+
+
+
+
   
-var nav = document.getElementById("myNav");
+  //typing 
 
 
-        var btns = document.getElementsByClassName("link");
+  
+  var i=0; // initialize counter at 0
+  text = "How can we help you?"; //var named text - to be typed
+  
 
-        for (var i = 0; i < btns.length; i++) {
-          btns[i].addEventListener("click", function() {
-            var current = document.getElementsByClassName("current");
-            current[0].className = current[0].className.replace(" current", "");
-            this.className += " current";
-          });
-        }
+
+
+  function typing(){
+    if(i<text.length){ //if var i is less than the length  of the the text var then append the character
+      document.getElementById("typo").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typing,90);
+    }
+  }
+
+  typing();
+
+
+  function highlight(element){
+  document.this.style.color = "black";
+  }
+
